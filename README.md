343e4ec56ac37233aa2df516d5c365071f050204
Alexandre Anastassov <a.anastassov@iaea.org>
2013-08-05 14:41:41 +0200

    before uploading to github

---
+++ README.md
@@ -0,0 +1,21 @@
+# nodejs-blog #
+
+Code based on __Introduction to Express__ with Raymond Camden (Nettuts+)
+
+For more specific information see README.md in public_html directory.
+
+### Git Quick Setup ###
+
+#### Create a new repository on the command line ####
+
+    touch README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git remote add origin https://github.com/anastass/nodejs-blog.git
+    git push -u origin master
+
+#### Push an existing repository from the command line ####
+
+    git remote add origin https://github.com/anastass/nodejs-blog.git
+    git push -u origin master
--- public_html/app.js
+++ public_html/app.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var app = express();
 
+var hbs = require('hbs');
+
+var blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.bodyParser());
+
+//app.get('/test/localhost', function(req, res) {
+//    res.send('localhost is running on port 3000');
+//});
+//
+//app.get('/test/readme', function(req, res) {
+//    res.sendfile('README.md');
+//});
+
 app.get('/', function(req, res) {
-    res.sendfile('./views/index.html');
+    res.render('index', {title: "MyBlog", entries: blogEngine.getBlogEntries()});
 });
 
-app.get('/test/', function(req, res) {
-    res.send('localhost is running on port 3000');
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.get('/article', function(req, res) {
+    res.render('article');
 });
 
 app.listen(3000);
